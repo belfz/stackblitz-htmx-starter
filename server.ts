62ef4360d09633b93fb578280b70106d12e048db
belfz <marcinbar1@gmail.com>
2024-04-23 09:56:39 +0200

    introduces "public" (static) directory

--- server.ts
+++ server.ts
@@ -1,18 +1,13 @@
 import express from 'express';
-import { resolve } from 'path';
 import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3010;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('static'));
+app.use(express.static('public'));
 
-const dumbDb = ['elo mordo'];
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+const dumbDb = ['initial item'];
 
 app.get('/todo', (req, res) => {
   res.send(dumbDb.map((el) => `<li>${el}</li>`).join(''));
@@ -22,12 +17,6 @@ app.post('/todo', (req, res) => {
   const { newTodo } = req.body;
   dumbDb.push(newTodo);
   res.send(`<li>${newTodo}</li>`);
-});
-
-app.get('/todo/next', (req, res) => {
-  res.send(`
-    <li>next</li>
-  `);
 });
 
 app.listen(port, () => {
